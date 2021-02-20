37c9a0de844fdfc56cd81c604ea2f66e65084657
james reantillo <james.reantillo@gmail.com>
2021-02-20 01:35:45 -0800

    add search functionality

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const auth = '563492ad6f917000010000013b11ed813284425a9e0d2817c567c4ee';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitButton = document.querySelector('.submit-btn');
+const form = document.querySelector('.search-form');
 let searchValue;
+
+//Event Listener
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
@@ -16,6 +27,27 @@ async function curatedPhotos() {
     }
   );
 
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement('div');
+    galleryImg.classList.add('gallery-img');
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: auth,
+      },
+    }
+  );
   const data = await dataFetch.json();
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement('div');
